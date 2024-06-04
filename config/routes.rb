dd2fb98f8022fcbd9bdf8ec612af396d291c2e02
ZarathustraSama <mr.gabriel@live.it>
2024-06-04 10:50:19 +0200

    Added resource for user#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :events, only: [:index]
+  resources :users, only: [:show]
 
   root to: 'events#index'
 end
