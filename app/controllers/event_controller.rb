c8630415fcb00f8aec785c0258c92fe0acfcd2d3
ZarathustraSama <mr.gabriel@live.it>
2024-06-04 10:03:25 +0200

    Fixed controller typo

--- app/controllers/event_controller.rb
+++
@@ -1,5 +0,0 @@
-class EventController < ApplicationController
-  def index
-    @events = Events.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :events, only: [:index]
 
-  root to: "events#index"
+  root to: 'events#index'
 end
