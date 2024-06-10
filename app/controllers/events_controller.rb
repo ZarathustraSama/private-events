5f669901e1df90807b26eebf3adee7fd36154cb5
ZarathustraSama <mr.gabriel@live.it>
2024-06-10 09:52:30 +0200

    Added #new, #create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    user = User.find(current_user.id)
+    if user.created_events.create!(event_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
   end
 end
