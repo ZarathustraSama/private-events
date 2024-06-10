775b028b327423c582386c1c75bf16570048fc92
ZarathustraSama <mr.gabriel@live.it>
2024-06-10 10:24:17 +0200

    Added #show route to events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
 
   root to: 'events#index'
