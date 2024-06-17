853efa533fe26fdd3d57540b6a14d1f2e18889ba
ZarathustraSama <mr.gabriel@live.it>
2024-06-17 10:25:25 +0200

    Created association/table for attendees of events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees, source: :event_attendee
+
   validates :name, presence: true
   validates :date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_075716) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_081948) do
+  create_table "event_attendees", force: :cascade do |t|
+    t.integer "attended_event_id"
+    t.integer "event_attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
