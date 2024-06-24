0b839e1da82e21094b5de173420e7c30996a1f5f
ZarathustraSama <mr.gabriel@live.it>
2024-06-24 11:08:36 +0200

    Simplified show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(current_user.id)
-    @events = user.created_events.all
+    @user = User.find(current_user.id)
   end
 end
